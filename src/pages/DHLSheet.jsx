18a00301e811e781f8ce6f280d72620150cdf9e4
Adnan <adnancyber7@gmail.com>
2026-01-02 05:00:49 +0000

    Manual code change

--- src/pages/DHLSheet.jsx
+++ src/pages/DHLSheet.jsx
@@ -718,7 +718,7 @@ const LoginScreen = ({ onLogin }) => {
               {activeTab === "admin" ? (
                 <motion.div
                   key="admin"
-                  initial={{ opacity: 0, x: -30 }}
+                  initial={{ opacity: 0, x: -50 }}
                   animate={{ opacity: 1, x: 0 }}
                   exit={{ opacity: 0, x: 30 }}
                   transition={{ duration: 0.4 }}
